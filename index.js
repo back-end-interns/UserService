8a6aed81417b7c5012fbb66b68db215c8c07df7f
Kat <44884378+MyNameIsKat@users.noreply.github.com>
2021-05-29 09:30:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+//refer to the middleware folder
+const errorRequest = require('./middlewares/requestErrorHandler');
 
 const db = require("./models");
 
@@ -12,6 +14,8 @@ app.use(express.urlencoded({extended: true}));
 
 //routes
 app.use("/user", require("./routers/user-router"));
+
+errorRequest(app);
 
 //used to create models or refer to the models
 db.sequelize.sync().then((req) => {
