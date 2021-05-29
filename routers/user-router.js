810fc6556faf6583933dd65e8adc3f1fcf40b42a
Kat <44884378+MyNameIsKat@users.noreply.github.com>
2021-05-29 09:29:07 +0800

    Update user-router.js

--- routers/user-router.js
+++ routers/user-router.js
@@ -5,17 +5,21 @@ const router = express.Router();
 //refer to controller
 const controller = require("../controllers/user-controller");
 
+//refer to middleware
+const {validator, schemaCreate} = require("../middlewares/validateRequest")
+const {schemaID} = require("../middlewares/validateRequest")
+
 //Create data
-router.post("/create", controller.createUser)
+router.post("/", validator.body(schemaCreate), controller.createUser)
 
 //Retrieve data
 router.get("/", controller.getData)
 
 //Update
-router.put("/update", controller.updateUser)
+router.put("/", validator.body(schemaID), controller.updateUser)
 
 //Delete
-router.delete("/delete", controller.deleteUser)
+router.delete("/", validator.body(schemaID), controller.deleteUser)
 
 //exports router
 module.exports = router
