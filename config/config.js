aead61a00e2384adfd2ec3a283ff288543cdf81f
Kat <44884378+MyNameIsKat@users.noreply.github.com>
2021-06-08 19:08:36 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config({path: './.env'});
+
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize('userservicedb', 'root', '', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.db_name, process.env.db_username, process.env.db_password, {
+  host: process.env.db_host,
   dialect: 'mysql'
 });
 
