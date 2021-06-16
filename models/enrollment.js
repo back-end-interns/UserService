97172d589c1bc52cd94689fcaa8753aca99bb5de
Kat <44884378+MyNameIsKat@users.noreply.github.com>
2021-06-16 19:40:53 +0800

    Create enrollment.js

---
+++ models/enrollment.js
@@ -0,0 +1,56 @@
+module.exports = (sequelize, DataTypes) => {
+    const Enrollment = sequelize.define('enrollment', {
+        enrollment_id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        lrn: {
+            type: DataTypes.BIGINT,
+            allowNull: false
+        },
+        department_id: {
+            type: DataTypes.BIGINT,
+            allowNull: false
+        },
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        section: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        yearlevel: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        lastschool: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        permanentbrgy: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        permanentprovince: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        presentbrgy: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        datem: {
+            type: DataTypes.DATEONLY,
+            allowNull: false
+        },
+        statuss: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+  
+    return Enrollment;
+  };
+  
