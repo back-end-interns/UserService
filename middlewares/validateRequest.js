836ba564a8f4696281787b07a4d481a53e5958b5
Kat <44884378+MyNameIsKat@users.noreply.github.com>
2021-06-25 22:07:17 +0800

    Update validateRequest.js

--- middlewares/validateRequest.js
+++ middlewares/validateRequest.js
@@ -3,11 +3,15 @@ const joi = require('joi');
 const validator = require('express-joi-validation').createValidator({passError: true});
 
 const schemaCreate = joi.object({
+    lrn: joi.number().required(),
     department_id: joi.number().required(),
+    password: joi.string().required(),
     firstname: joi.string().required(),
     middlename: joi.string().required(),
     lastname: joi.string().required(),
     address: joi.string().required(),
+    permanentbrgy: joi.string().required(),
+    permanentprovince: joi.string().required(),
     contact_no: joi.number().required(),
     password: joi.string().required(),
     person_to_contact: joi.string().required(),
@@ -18,26 +22,13 @@ const schemaCreate = joi.object({
     dateOfBirth: joi.string().required(),
     placeOfBirth: joi.string().required(),
     religion: joi.string().required(),
-    guardian: joi.string().required()
-})
-
-const schemaEnroll = joi.object({
-    department_id: joi.number().required(),
-    lrn: joi.number().required(),
-    age: joi.number().required(),
-    section: joi.string().required(),
-    yearlevel: joi.number().required(),
-    lastschool: joi.string().required(),
-    permanentbrgy: joi.string().required(),
-    permanentprovince: joi.string().required(),
-    presentbrgy: joi.string().required(),
-    datem: joi.string().required(),
-    statuss: joi.string().required()
+    guardian: joi.string().required(),
+    status: joi.string().default("pending")
 })
 
 const schemaID = joi.object({
     id: joi.number().required()
 })
 
-module.exports = {validator, schemaCreate, schemaID, schemaEnroll};
+module.exports = {validator, schemaCreate, schemaID};
 
